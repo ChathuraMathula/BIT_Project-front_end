9bfc894e6c921476f7e781f6d35ef4a0dc8e9192
chathura ekanayake <chathuramathula@gmail.com>
2023-02-14 21:19:57 +0530

    customer sign up form halfly implemented

--- webpack.config.js
+++ webpack.config.js
@@ -34,17 +34,7 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
-      },{
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'svg-url-loader',
-            options: {
-              limit: 10000,
-            },
-          },
-        ],
       }
-    ],
+    ]
   },
 };
