1c810787ec4a7d7e3404217c417f9e398a9b4771
chathura ekanayake <chathuramathula@gmail.com>
2023-03-03 13:40:58 +0530

    admin update photographer profile added

--- src/components/user/admin/AdminPhotographerProfile.js
+++ src/components/user/admin/AdminPhotographerProfile.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { UserLoginContext } from "../../../context/Context";
 import FormHeading from "../../UI/form/FormHeading";
+import UserProfileDetails from "../UserProfileDetails";
 import UserProfilePassword from "../UserProfilePassword";
 import UserProfilePicture from "../UserProfilePicture";
 
@@ -8,11 +9,13 @@ const AdminPhotographerProfile = (props) => {
   const login = useContext(UserLoginContext);
 
   if (login.user.name === "admin") {
+    const user = {name: "photographer", role: "photographer"};
     return (
       <>
         <FormHeading>PHOTOGRAPHER PROFILE</FormHeading>
-        <UserProfilePicture user={login.user} />
-        <UserProfilePassword user={login.user} />
+        <UserProfilePicture user={user} />
+        <UserProfileDetails user={user} />
+        <UserProfilePassword user={user} />
       </>
     );
   }
