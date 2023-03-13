73b728546cc8262b69cec4abaf4d1cb9d50cce8e
chathura ekanayake <chathuramathula@gmail.com>
2023-03-13 22:11:23 +0530

    custmer payment details adding feature inplemented partially

---
+++ src/components/UI/SVG/UploadSVG.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+const UploadSVG = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="463"
+        height="463"
+        viewBox="0 0 463 463"
+      >
+        <g id="Group_2" data-name="Group 2" transform="translate(-1424 858)">
+          <rect
+            id="Rectangle_28"
+            data-name="Rectangle 28"
+            width="463"
+            height="463"
+            transform="translate(1424 -858)"
+            fill="none"
+          />
+          <path
+            id="Path_52"
+            data-name="Path 52"
+            d="M0,338"
+            transform="translate(1655.5 -795.5)"
+            fill="none"
+            stroke="#000a1a"
+            stroke-width="55"
+          />
+          <g id="Group_1" data-name="Group 1" transform="translate(-32 -16)">
+            <g
+              id="Polygon_1"
+              data-name="Polygon 1"
+              transform="translate(1546 -733)"
+              fill="#adc2eb"
+            >
+              <path
+                d="M 281.6626586914062 122.5 L 1.337352871894836 122.5 L 141.4999694824219 0.6625047922134399 L 281.6626586914062 122.5 Z"
+                stroke="none"
+              />
+              <path
+                d="M 141.4999694824219 1.324981689453125 L 2.6746826171875 122 L 280.3253173828125 122 L 141.4999694824219 1.324981689453125 M 141.4999694824219 0 L 283 123 L 0 123 L 141.4999694824219 0 Z"
+                stroke="none"
+                fill="#adc2eb"
+              />
+            </g>
+            <g
+              id="Rectangle_29"
+              data-name="Rectangle 29"
+              transform="translate(1546 -611)"
+              fill="#adc2eb"
+              stroke="#adc2eb"
+              stroke-width="1"
+            >
+              <rect width="283" height="46" stroke="none" />
+              <rect x="0.5" y="0.5" width="282" height="45" fill="none" />
+            </g>
+            <g
+              id="Rectangle_30"
+              data-name="Rectangle 30"
+              transform="translate(1546 -553)"
+              fill="#adc2eb"
+              stroke="#adc2eb"
+              stroke-width="1"
+            >
+              <rect width="283" height="24" stroke="none" />
+              <rect x="0.5" y="0.5" width="282" height="23" fill="none" />
+            </g>
+          </g>
+        </g>
+      </svg>
+    </>
+  );
+};
+
+export default UploadSVG;
--- src/components/user/customer/CustomerPaymentDetails.js
+++ src/components/user/customer/CustomerPaymentDetails.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import CalenderDateState from "../../UI/calender/CalenderDateState";
+import FormInput from "../../UI/form/FormInput";
+import UploadSVG from "../../UI/SVG/UploadSVG";
 import "./CustomerPaymentDetails.css";
 
 /**
@@ -10,9 +12,13 @@ import "./CustomerPaymentDetails.css";
  * @returns
  */
 const CustomerPaymentDetails = (props) => {
+  let noticeString =
+    "Please upload a scanned copy of your bank payment slip. Make sure the bank seal is visible clearly.";
   const [hours, setHours] = useState("");
   const [minutes, setMinutes] = useState("");
   const [seconds, setSeconds] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("bank");
+  const [screenshotNotice, setScreenshotNotice] = useState(noticeString);
   const [photographer, setPhotographer] = useState({});
 
   useEffect(() => {
@@ -53,6 +59,18 @@ const CustomerPaymentDetails = (props) => {
         }
       });
   }, []);
+
+  const onClickByBankHandler = (e) => {
+    setPaymentMethod("bank");
+    noticeString =
+      "Please upload a scanned copy of your bank payment slip. Make sure the bank seal is visible clearly.";
+    setScreenshotNotice(noticeString);
+  };
+  const onClickByOnlineBankingHandler = (e) => {
+    setPaymentMethod("online banking");
+    noticeString = "Please upload a screenshot of your payment.";
+    setScreenshotNotice(noticeString);
+  };
 
   return (
     <>
@@ -116,8 +134,59 @@ const CustomerPaymentDetails = (props) => {
         </div>
         <div className="customer-payment-details__bank-details">
           <div>Please pay advance payment to,</div>
-          <div><span>Bank: </span>{photographer.bankName}</div>
-          <div><span>Account No: </span>{photographer.bankAccountNo}</div>
+          <div>
+            <span>Bank: </span>
+            {photographer.bankName}
+          </div>
+          <div>
+            <span>Account No: </span>
+            {photographer.bankAccountNo}
+          </div>
+        </div>
+      </div>
+      <div className="customer-payment-details-payment__container">
+        <div className="customer-payment-details-payment__title">
+          PAYMENT DETAILS
+        </div>
+        <div className="customer-payment-details-payment__method-notice">
+          Select Your Payment Method
+        </div>
+        <div className="customer-payment-details-payment-method__container">
+          <div
+            className={
+              paymentMethod === "bank" ? "selected-payment-method" : null
+            }
+            onClick={onClickByBankHandler}
+          >
+            By Bank
+          </div>
+          <div
+            className={
+              paymentMethod === "online banking"
+                ? "selected-payment-method"
+                : null
+            }
+            onClick={onClickByOnlineBankingHandler}
+          >
+            By Online Banking
+          </div>
+        </div>
+        <div>
+          {paymentMethod === "bank" ? (
+            <FormInput placeholder="Example: POLGAHAWELA">
+              Paid Bank Branch:
+            </FormInput>
+          ) : null}
+          <FormInput placeholder="Example: 10,000.00">Paid Amount:</FormInput>
+          <FormInput placeholder="DD / MM / YYYY">Paid Date:</FormInput>
+          <FormInput placeholder="HH:MM">Paid Time:</FormInput>
+          <div>{screenshotNotice}</div>
+          <div>
+            <label className="customer-payment-details-screenshot__upload">
+              <UploadSVG />
+              <input type="file" />
+            </label>
+          </div>
         </div>
       </div>
     </>
